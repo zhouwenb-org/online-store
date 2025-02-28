b28436d6661fb3b1a24ffe8876261222b078605f
changmen.zx <changmen.zx@taobao.com>
2025-02-28 17:48:58 +0800

    E.1.1.4.1 单行超过120个字符

---
+++ src/main/java/com/example/onlinestore/service/impl/ItemServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.onlinestore.service.impl;
+
+import com.example.onlinestore.bean.Item;
+import com.example.onlinestore.entity.ItemEntity;
+import com.example.onlinestore.mapper.ItemMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class ItemServiceImpl extends CommonItemService{
+
+    @Autowired
+    private ItemMapper itemMapper;
+
+    @Override
+    public void addItem(String userId, Item item) {
+
+    }
+
+
+    private ItemEntity buildItemEntity(String userId, String name, String description, String image, String secondaryName, String pingJia, Long skuId, Map<String, Map<String, String>> itemAttributes, Map<String, Map<String, String>> itemExtensions) {
+        ItemEntity entity = new ItemEntity();
+        entity.setName(name);
+        entity.setDescription(description);
+        entity.setImage(image);
+        entity.setSecondaryName(secondaryName);
+        entity.setSkuId(skuId);
+        return entity;
+    }
+}
