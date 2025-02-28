5c47f90e1a516e36b20ed8f476e178ef2a83a966
changmen.zx <changmen.zx@taobao.com>
2025-02-28 17:36:41 +0800

    E.1.1.3.1 代码存在魔法值

--- src/main/java/com/example/onlinestore/service/impl/CommonItemService.java
+++ src/main/java/com/example/onlinestore/service/impl/CommonItemService.java
@@ -1,15 +1,23 @@
 package com.example.onlinestore.service.impl;
 
 import com.example.onlinestore.bean.Item;
+import com.example.onlinestore.bean.VirtualItem;
 import com.example.onlinestore.entity.ItemEntity;
 import com.example.onlinestore.service.ItemService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.cglib.beans.BeanCopier;
 
 public abstract class CommonItemService implements ItemService {
+
         protected Item convertToItem(ItemEntity itemEntity) {
+            if (StringUtils.endsWithIgnoreCase(itemEntity.getName(), "test-item")){
+                return new VirtualItem();
+            }
+
             Item item = new Item();
             BeanCopier copier = BeanCopier.create(ItemEntity.class, Item.class, false);
             copier.copy(itemEntity, item, null);
+
             return item;
         }
 
