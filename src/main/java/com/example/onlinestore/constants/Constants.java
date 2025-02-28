37e82d511605be201a2f0690f803b83f52de3fd3
changmen.zx <changmen.zx@taobao.com>
2025-02-28 17:16:24 +0800

    E.1.1.3.2 long型常量不能以l结尾

--- src/main/java/com/example/onlinestore/constants/Constants.java
+++ src/main/java/com/example/onlinestore/constants/Constants.java
@@ -2,4 +2,5 @@ package com.example.onlinestore.constants;
 
 public class Constants {
     public static final String successCode = "success";
+    public static final long successNumCode = 200l;
 }
